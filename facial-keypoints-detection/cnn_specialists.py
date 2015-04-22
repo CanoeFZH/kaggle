f3304586cf1eada78280a54a8d76ccb6b9e5cf82
canoe <canoe@canoe-ubuntu.(none)>
2015-04-22 21:07:49 +0800

    ensemble

--- facial-keypoints-detection/cnn_specialists.py
+++ facial-keypoints-detection/cnn_specialists.py
@@ -231,8 +231,9 @@ def predict_specialists():
 
         sp[sp_name] = pY
         i += 1
+    return Y_ALL, sp
     
-def ensemble():
+def ensemble(Y_ALL, sp):
     NAME_PREFIX = "left_eye_center, right_eye_center, left_eye_inner_corner, left_eye_outer_corner, right_eye_inner_corner, right_eye_outer_corner, left_eyebrow_inner_end, left_eyebrow_outer_end, right_eyebrow_inner_end, right_eyebrow_outer_end, nose_tip, mouth_left_corner, mouth_right_corner, mouth_center_top_lip, mouth_center_bottom_lip".split(', ')
     #ensemble
     names = {}
